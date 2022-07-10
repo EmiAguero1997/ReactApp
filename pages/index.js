ccc6e4e40d3c3b56197e19dc363200854caf3776
Emiliano Agüero <eaguero@folcode.com>
2022-07-10 17:52:14 -0300

    card added

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,14 @@ export default function Home() {
   const goToText = () =>{
     router.push('/test');
   }
+  const goToCard = ()=>{
+    router.push('/card')
+  }
   return (
     <div>
       <h1>Hola mundo!</h1>
       <button onClick={goToText}>Go to Test</button>
+      <button onClick={goToCard}>Go to Card</button>
     </div>
   )
 }
