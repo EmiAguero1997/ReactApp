d5a8e48d865712ae239588bae27383043f9c8b1b
Emiliano Agüero <eaguero@folcode.com>
2022-07-31 21:55:24 -0300

    datea fetch made and shown on a select

--- pages/renderer.js
+++ pages/renderer.js
@@ -1,72 +1,113 @@
 import Rendered from "./rendered";
-import Card from './card';
-import {useState, useEffect} from 'react';
-import styles from './styles.module.css';
+import Card from "./card";
+import { useState, useEffect } from "react";
+import styles from "./styles.module.css";
 import { TextField } from "@mui/material";
-import {CardHeader} from "@mui/material";
-import Footer from './footer';
+import { CardHeader } from "@mui/material";
+import { FormControl } from "@mui/material";
+import { Select } from "@mui/material";
+import { MenuItem } from "@mui/material";
+import {Button} from '@mui/material';
+import Footer from "./footer";
 
-function Renderer(){
+function Renderer() {
+  const axios = require("axios").default;
+  const [cardTitle, setCardTitle] = useState();
+  const [cardDesc, setCardDesc] = useState();
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+  const [apiData, setApiData] = useState();
+  const [pokemon,setPokemon] = useState('Listado');
 
-    const axios = require('axios').default;
-    const [cardTitle, setCardTitle] = useState();
-    const [cardDesc, setCardDesc] = useState();
-    const [counter, setCounter] = useState(0);
-    const [counter2, setCounter2] = useState(0);
-    const [disabled, setDisabled] = useState(false);
-    var data;
+  function getData() {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
+      setApiData(response.data.results);
+      setPokemon(response.data.results[0].name);
+    })
+    .catch((error) =>{
+        console.log(error);
+    })
+  }
 
-    function getData(){
-        axios.get('https://pokeapi.co/api/v2/pokemon/ditto').then(response =>{
-            
-            data = response.data;
-            console.log('data: ',data);
+  useEffect(() => {
+    counter2 == 5 &&
+      (alert("You clicked 5 times"), setCounter2(0), disableBtn());
+  }, [counter2]);
 
-        })
-    }
+  function disableBtn() {
+    setDisabled(true);
+  }
 
-    useEffect(()=>{
-        counter2==5 && (alert('You clicked 5 times'), setCounter2(0), disableBtn());
-    }, [counter2])
+  const handleChange = (event) => {
+    setCardTitle(event.target.value);
+  };
 
-    function disableBtn(){
-        setDisabled(true);
-    }
+  function handleDescChange(event) {
+    setCardDesc(event.target.value);
+  }
 
-    const handleChange = event =>{
-        setCardTitle(event.target.value);
-    }
+  function handleSelectChange(event){
+    setPokemon(event.target.value);
+  }
 
-    function handleDescChange(event){
-        setCardDesc(event.target.value);
-    }
-
-    function handleClick(){
-        console.log('click');
-    }
-
-    const clickCounter = num => {
-        setCounter(current => current + num);
-        setCounter2(current => current + num);
-    }
-    return(
-        <>
-            <CardHeader title="Emiserv Custom Bikes" className={styles.header}>   
-            </CardHeader>
-            <div>
-                <div className={styles.textFields}>
-                    <TextField type="text" className={styles.title} placeholder="Ingrese el titulo de la card" onChange={handleChange}></TextField>
-                    <b></b>
-                    <TextField type="text" className={styles.title} placeholder="Ingrese la descripcion de la card" onChange={handleDescChange}></TextField>
-                </div>
-                <button onClick={()=>getData()}>Get data from API</button>
-                 <div className={styles.textFields}>
-                    <Card disabled={disabled} clickCounter={clickCounter} title={cardTitle? cardTitle : 'Honda CB 750'} description={cardDesc? cardDesc : 'With a powerful and reliable engine, the Honda CB 750 CC3 bike is one of the most choosed base bikes for Cafe racer bikes enthusiasts.'}/>
-                 </div>
-                <h2 className={styles.textFields}>Click counter: {counter}</h2>
-            </div>
-            <Footer></Footer>
-        </>
-    )
+  const clickCounter = (num) => {
+    setCounter((current) => current + num);
+    setCounter2((current) => current + num);
+  };
+  return (
+    <>
+      <CardHeader
+        title="Emiserv Custom Bikes"
+        className={styles.header}
+      ></CardHeader>
+      <div>
+        <div className={styles.textFields}>
+          <TextField
+            type="text"
+            className={styles.title}
+            placeholder="Ingrese el titulo de la card"
+            onChange={handleChange}
+          ></TextField>
+          <b></b>
+          <TextField
+            type="text"
+            className={styles.title}
+            placeholder="Ingrese la descripcion de la card"
+            onChange={handleDescChange}
+          ></TextField>
+        </div>
+        <section>
+          <Button variant="contained" onClick={() => getData()}>Get data from API</Button>
+          {apiData && (
+            <FormControl>
+              <Select
+                value={pokemon}
+                onChange={handleSelectChange}
+              >
+                {apiData.map((element) => (
+                  <MenuItem value={element.name} key={element.name}>{element.name}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          )}
+        </section>
+        <div className={styles.textFields}>
+          <Card
+            disabled={disabled}
+            clickCounter={clickCounter}
+            title={cardTitle ? cardTitle : "Honda CB 750"}
+            description={
+              cardDesc
+                ? cardDesc
+                : "With a powerful and reliable engine, the Honda CB 750 CC3 bike is one of the most choosed base bikes for Cafe racer bikes enthusiasts."
+            }
+          />
+        </div>
+        <h2 className={styles.textFields}>Click counter: {counter}</h2>
+      </div>
+      <Footer></Footer>
+    </>
+  );
 }
-export default Renderer
+export default Renderer;
